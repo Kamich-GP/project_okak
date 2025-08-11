1f8585e702ed2138fb4c1f46d4537f01ecc5d3e6
Kamich-GP <yurochkagameplus@gmail.com>
2025-08-11 21:27:05 +0500

    database

--- buttons.py
+++ buttons.py
@@ -11,3 +11,17 @@ def num_button():
     # Добавляем кнопки в пространство
     kb.add(but1)
     return kb
+
+
+# Кнопки вывода товаров
+def main_menu(products):
+    # Создаем пространство
+    kb = types.InlineKeyboardMarkup(row_width=2)
+    # Создаем сами кнопки
+    cart = types.InlineKeyboardButton(text='Корзина🛒', callback_data='cart')
+    all_products = [types.InlineKeyboardButton(text=f'{i[1]}', callback_data=i[0])
+                    for i in products]
+    # Добавляем кнопки в пространство
+    kb.add(*all_products)
+    kb.row(cart)
+    return kb
--- database.py
+++ database.py
@@ -9,6 +9,10 @@ sql = connection.cursor()
 
 # Создание таблиц
 sql.execute('CREATE TABLE IF NOT EXISTS users (tg_id INTEGER, name TEXT, num TEXT);')
+sql.execute('CREATE TABLE IF NOT EXISTS products (pr_id INTEGER PRIMARY KEY AUTOINCREMENT, '
+            'pr_name TEXT, pr_des TEXT, pr_count INTEGER, pr_price INTEGER, pr_photo TEXT);')
+sql.execute('CREATE TABLE IF NOT EXISTS cart (tg_id INTEGER, user_product TEXT, '
+            'user_pr_amount INTEGER);')
 
 
 ## Методы пользователя ##
@@ -24,3 +28,52 @@ def check_user(tg_id):
         return True
     else:
         return False
+
+## Методы продуктов ##
+# Вывод всех товаров из БД
+def get_all_pr():
+    return sql.execute('SELECT * FROM products;').fetchall()
+
+# Вывод товаров на кнопки
+def get_pr_buttons():
+    return [i[:2] for i in get_all_pr() if i[3] > 0]
+
+# Вывод определенного товара
+def get_exact_pr(pr_id):
+    return sql.execute('SELECT * FROM products WHERE pr_id=?;', (pr_id,)).fetchone()
+
+# Вывод цены товара по названию
+def get_price(pr_name):
+    return sql.execute('SELECT pr_price FROM products WHERE pr_name=?;', (pr_name,)).fetchone()
+
+## Методы корзины ##
+# Добавление в корзину
+def add_to_cart(tg_id, user_product, user_pr_amount):
+    sql.execute('INSERT INTO cart VALUES (?, ?, ?);', (tg_id, user_product, user_pr_amount))
+    # Фиксируем изменения
+    connection.commit()
+
+# Очистка корзины
+def clear_cart(tg_id):
+    sql.execute('DELETE FROM cart WHERE tg_id=?;', (tg_id,))
+    # Фиксируем изменения
+    connection.commit()
+
+# Оформление заказа
+def make_order(tg_id):
+    # Получаем инфу о корзина пользователя
+    cart_items = sql.execute('SELECT user_product, user_pr_amount FROM cart WHERE tg_id=?;',
+                             (tg_id,)).fetchall()
+    results = []
+
+    for product_name, cart_amount in cart_items:
+        # Получаем кол-во товаров на СКЛАДЕ
+        stock = sql.execute('SELECT pr_count FROM products WHERE pr_name=?;',
+                            (product_name,)).fetchone()[0]
+        new_count = stock - cart_amount
+        sql.execute('UPDATE products SET pr_count=? WHERE pr_name=?;', (new_count, product_name))
+        results.append(new_count)
+
+    # Фиксируем изменения
+    connection.commit()
+    return results
